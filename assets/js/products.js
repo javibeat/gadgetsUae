d4e6147cff5f7ec5ff14884df83bf9a0ce85f96b
javibeat <javibeat@gmail.com>
2025-11-27 10:20:52 +0400

    SEO

--- assets/js/products.js
+++ assets/js/products.js
@@ -59,9 +59,9 @@ const products = [
     title: 'Nintendo Switch 2 Console - International Version [New 2025 Model]',
     category: 'Video Games',
     image: 'assets/images/nintendo/switch2/1.jpg',
-    price: 'AED 2,144.80',
-    original: 'AED 2,899.00',
-    discount: '-26%',
+    price: 'AED 1,677.00',
+    original: 'AED 1,767.00',
+    discount: '-5%',
     url: 'https://amzn.to/4mU9Kui',
     review: 'https://www.youtube.com/watch?v=K3CR6RiWS2U&t=33s',
     gallery: [
@@ -83,7 +83,9 @@ const products = [
     title: 'Nintendo Switch 2 Pro Controller',
     category: 'Video Games / Accessories',
     image: 'assets/images/nintendo/procontroller2/1.jpg',
-    price: 'AED 347.97',
+    price: 'AED 330.00',
+    original: 'AED 347.00',
+    discount: '-5%',
     url: 'https://amzn.to/4eud8s0',
     review: 'https://www.youtube.com/shorts/MLg8YJbazoM',
     gallery: [
@@ -101,7 +103,7 @@ const products = [
     title: 'SanDisk microSD Express 256GB for Nintendo Switch 2',
     category: 'Video Games / Storage',
     image: 'assets/images/nintendo/sd/sd1.jpg',
-    price: 'AED 223.03',
+    price: 'AED 249.00',
     url: 'https://amzn.to/3Tmgofm',
     gallery: [
       'assets/images/nintendo/sd/sd1.jpg',
